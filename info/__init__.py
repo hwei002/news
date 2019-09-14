23636f42e206b41ce0258df5d682ebffbb8cd7ce
DaZhuWei <huazhou.wei@gmail.com>
2019-09-14 10:33:15 +0800

    封装app进create_app工厂方法，在manage中调用，传入environment参数并用app接收返回的配置类

--- info/__init__.py
+++ info/__init__.py
@@ -10,15 +10,26 @@ from flask_session import Session
 from config import config
 
 
-app = Flask(__name__)
-# 加载配置
-app.config.from_object(config["development"])
 # 初始化 SQL 数据库
-db = SQLAlchemy(app)
-# 初始化 redis 存储对象
-redis_store = StrictRedis(host=config["development"].REDIS_HOST,
-                          port=config["development"].REDIS_PORT)
-# 开启当前项目 CSRF 保护（只做验证，cookie中csrf_token和表单中csrf_token需要手动实现）
-CSRFProtect(app)
-# 设置用Session将 app 中数据保存到指定位置
-Session(app)
+# Flask中很多扩展都可以先创建扩展对象，然后等初始化所需参数出现后，再调用init_app方法完成初始化
+db = SQLAlchemy()
+
+
+def create_app(environment):
+    app = Flask(__name__)
+    # 加载配置
+    app.config.from_object(config[environment])
+
+    # 传入app当参数，完成db初始化
+    db.init_app(app)
+
+    # 初始化 redis 存储对象
+    redis_store = StrictRedis(host=config[environment].REDIS_HOST,
+                              port=config[environment].REDIS_PORT)
+    # 开启当前项目 CSRF 保护（只做验证，cookie中csrf_token和表单中csrf_token需要手动实现）
+    CSRFProtect(app)
+    # 设置用Session将 app 中数据保存到指定位置
+    Session(app)
+
+    return app  # 保留db在函数内 & “return app, db” --- 同样奏效
+
--- manage.py
+++ manage.py
@@ -3,9 +3,10 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 # app/sql/redis/csrf/session属业务逻辑，放info包。
 # 后续static/js/css以及其他代码都放info，而manage仅当程序入口
-from info import app, db
+from info import create_app, db
 
 
+app = create_app("development")
 # 设置Flask-Script，用命令行运行【python manage.py runserver】
 manager = Manager(app)
 # 设置Migrate数据库迁移功能：关联 app 与 db，并把迁移命令添加到Script
