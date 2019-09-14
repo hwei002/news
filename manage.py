cae2e5217c66b70e3ea03a9fc63d46a925e0c690
DaZhuWei <huazhou.wei@gmail.com>
2019-09-14 08:39:48 +0800

    集成Flask-Script实现命令行运行

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ from flask_session import Session
 # flask中Session --- 将浏览器来的数据加密存cookie返回浏览器，浏览器下次带cookie来再解密
 # flask中session --- 用于在视图函数中作为键值对的字典名，如session["name"] = "laowang"
 # flask_session中Session --- 可以指定数据保存位置，如redis/sqlalchemy/mongodb/memcached等
+from flask.ext.script import Manager
 
 
 class Config(object):
@@ -21,13 +22,14 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
-    # 指定浏览器数据保存位置为 redis，并配置所需参数
+    # 设置Session：指定浏览器数据保存位置为 redis，并配置所需参数
     SESSION_TYPE = "redis"
     SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 指定保存用的redis对象
     SESSION_USE_SIGNER = True  # 开启session签名（session使用签名，提高加密性）
     SESSION_PERMANENT = False  # 设置session需要过期（取消其默认的永不过期）
     PERMANENT_SESSION_LIFETIME = 86400 * 2  # 设置过期时间为 2 天（默认是 31 天）
     SECRET_KEY = "HfQ/Co3pMKAlEml4wlbbBgIoGoFhQUU922Hh7dq4d51YZCislqBKyHBjTXf+74Lk"
+
 
 app = Flask(__name__)
 # 加载配置
@@ -40,6 +42,8 @@ redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 # 设置用Session将 app 中数据保存到指定位置
 Session(app)
+# 设置Flask-Script，用命令行运行【python manage.py runserver】
+manager = Manager(app)
 
 
 @app.route('/')
@@ -49,5 +53,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
 
