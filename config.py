02afc9cdf66e64f649493d537b5e1cb1f6e6a41f
DaZhuWei <huazhou.wei@gmail.com>
2019-09-14 09:57:29 +0800

    为不同环境抽自定义配置类 & 通过环境名称字典映射获取指定配置类

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ from redis import StrictRedis
 
 
 class Config(object):
-    """项目的所有配置"""
-    DEBUG = True
-
+    """项目基本配置，用来被后续不同模式继承"""
     # 为 SQL 数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/news"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -21,3 +19,24 @@ class Config(object):
     PERMANENT_SESSION_LIFETIME = 86400 * 2  # 设置过期时间为 2 天（默认是 31 天）
     SECRET_KEY = "HfQ/Co3pMKAlEml4wlbbBgIoGoFhQUU922Hh7dq4d51YZCislqBKyHBjTXf+74Lk"
 
+
+class DevelopmentConfig(Config):
+    """开发环境下的配置"""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """生产环境下的配置"""
+    DEBUG = False  # 除debug而外，生产环境下的redis_host也不应该是127.0.0.1 --- 后续再改
+
+
+class TestingConfig(Config):
+    """单元测试环境下的配置"""
+    DEBUG = True
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "testing": TestingConfig,
+}
--- info/__init__.py
+++ info/__init__.py
@@ -7,16 +7,17 @@ from flask_session import Session
 # flask中Session --- 将浏览器来的数据加密存cookie返回浏览器，浏览器下次带cookie来再解密
 # flask中session --- 用于在视图函数中作为键值对的字典名，如session["name"] = "laowang"
 # flask_session中Session --- 可以指定数据保存位置，如redis/sqlalchemy/mongodb/memcached等
-from config import Config
+from config import config
 
 
 app = Flask(__name__)
 # 加载配置
-app.config.from_object(Config)
+app.config.from_object(config["development"])
 # 初始化 SQL 数据库
 db = SQLAlchemy(app)
 # 初始化 redis 存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+redis_store = StrictRedis(host=config["development"].REDIS_HOST,
+                          port=config["development"].REDIS_PORT)
 # 开启当前项目 CSRF 保护（只做验证，cookie中csrf_token和表单中csrf_token需要手动实现）
 CSRFProtect(app)
 # 设置用Session将 app 中数据保存到指定位置
