1b20da1409eb0b1fea0c22f566945b24157c5a33
DaZhuWei <huazhou.wei@gmail.com>
2019-09-19 23:30:13 +0800

    新闻详情页的新闻主体部分的数据展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,4 +1,4 @@
-from flask import current_app, render_template, g
+from flask import current_app, render_template, g, abort
 from info import constants
 from info.models import News
 from info.modules.news import news_blu
@@ -8,15 +8,10 @@ from info.utils.common import user_login_data
 @news_blu.route('/<int:news_id>')  # int冒号后不能加空格！！
 @user_login_data
 def news_detail(news_id):
+
     # 右上角功能区的实现：【登录/注册】or【用户头像/昵称/退出】
-    # user_id = session.get("user_id", None)  # 尝试获取当前登录用户的user_id
-    # user = None  # 先定义，保证后续data中的使用不会报 undefined error
-    # if user_id:
-    #     try:
-    #         user = User.query.get(user_id)  # 通过 user_id 获取用户信息
-    #     except Exception as e:
-    #         current_app.logger.error(e)
     user = g.user
+
     # 右侧新闻点击排行功能的实现
     top_click_news = []
     try:
@@ -24,10 +19,22 @@ def news_detail(news_id):
     except Exception as e:
         current_app.logger.error(e)
     top_click_news = [news.to_basic_dict() for news in top_click_news]  # 每条新闻从对象转字典，方便前端调用
+
+    # 新闻主体部分的展示
+    news = None
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+    if not news:
+        abort(404)  # 数据未找到有一个统一的404页面，后续再去实现此功能
+    news.clicks += 1  # 更新点击次数
+
     # 把数据进行汇总，当参数传给模板，供渲染时使用
     data = {
         "user": user.to_dict() if user else None,  # 将user转为字典形式传给前端html模板，供 js 分支判断使用
         "top_click_news": top_click_news,
+        "news": news.to_dict(),
     }
     return render_template('news/detail.html', data=data)
 
