940f47f89aeffecbe00ec2bc84235bb91da3b250
DaZhuWei <huazhou.wei@gmail.com>
2019-09-19 20:31:33 +0800

    从首页index.html和新闻详情页detail.html中，抽取基类base.html

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -4,4 +4,6 @@ from info.modules.news import news_blu
 
 @news_blu.route('/<int:news_id>')  # int冒号后不能加空格。。。
 def news_detail(news_id):
-    return render_template('news/detail.html')
+    data = {}
+    return render_template('news/detail.html', data=data)
+
