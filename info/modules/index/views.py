9d8ef768827983f0118fe1af836cca938f3298a0
DaZhuWei <huazhou.wei@gmail.com>
2019-09-19 10:17:07 +0800

    实现首页新闻列表的后端代码

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,50 @@
 # 3.导入蓝图对象，并用它注册路由，装饰视图函数
+from flask import render_template, current_app, session, request, jsonify
 from info import constants
 from info.models import User, News
-from flask import render_template, current_app, session
+from info.utils.response_code import RET
 from . import index_blu
+
+
+@index_blu.route('/news_list')
+def news_list():
+
+    # 1. 获取url中？后面的参数：新闻分类cid，页码page，每页条数per_page
+    cid = request.args.get("cid", "1")
+    page = request.args.get("page", "1")
+    per_page = request.args.get("per_page", "10")
+
+    # 2. 校验参数
+    try:
+        cid = int(cid)
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3. 查询数据
+    filters = [News.category_id==cid] if cid != 1 else []  # cid=1表示“最新”新闻，无需过滤。。。而cid=2,3,4……才需要过滤
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    # 4. 通过paginate对象获取：当前页码、总页数、当前页的所有新闻列表
+    current_page = paginate.page
+    total_page = paginate.pages
+    current_page_news = paginate.items
+    current_page_news = [news.to_basic_dict() for news in current_page_news]
+
+    # 5. 生成数据并返回
+    data = {
+        "current_page": current_page,
+        "total_page": total_page,
+        "current_page_news": current_page_news,
+    }
+
+    return jsonify(errno=RET.OK, errmsg="OK", data=data)
 
 
 @index_blu.route('/')
