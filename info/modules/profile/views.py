2f6260c7f6222805feb4619fd77404004ba2aff1
DaZhuWei <huazhou.wei@gmail.com>
2019-09-21 19:28:04 +0800

    【个人中心里的基本资料模块数据显示和修改更新】的前后端代码实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,6 +1,25 @@
-from flask import g, redirect, render_template
+from flask import g, redirect, render_template, request, jsonify
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
+
+
+@profile_blu.route('/base_info', methods=["GET", "POST"])
+@user_login_data
+def base_info():
+    user = g.user
+    if request.method == "GET":
+        return render_template("news/user_base_info.html", data={"user": user.to_dict()})
+    elif request.method == "POST":
+        nick_name = request.json.get("nick_name", None)
+        signature = request.json.get("signature", None)
+        gender = request.json.get("gender", None)
+        if not all([nick_name, signature]) or gender not in ["MAN", "WOMAN"]:
+            return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+        user.nick_name = nick_name
+        user.signature = signature
+        user.gender = gender
+        return jsonify(errno=RET.OK, errmsg="个人基本信息修改成功")
 
 
 @profile_blu.route('/info')
--- info/static/news/js/user_base_info.js
+++ info/static/news/js/user_base_info.js
@@ -4,22 +4,42 @@ function getCookie(name) {
 }
 
 $(function () {
-
     $(".base_info").submit(function (e) {
-        e.preventDefault()
-
-        var signature = $("#signature").val()
-        var nick_name = $("#nick_name").val()
-        var gender = $(".gender").val()
-
+        e.preventDefault();
+        var signature = $("#signature").val();
+        var nick_name = $("#nick_name").val();
+        var gender = $(".gender").val();
         if (!nick_name) {
-            alert('请输入昵称')
+            alert('请输入昵称');
             return
         }
         if (!gender) {
             alert('请选择性别')
         }
-
-        // TODO 修改用户信息接口
+        // 修改用户信息接口
+        var params = {
+            "signature": signature,
+            "nick_name": nick_name,
+            "gender": gender
+        };
+        $.ajax({
+            url: "/user/base_info",
+            type: "post",
+            contentType: "application/json",
+            data: JSON.stringify(params),
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            success: function (resp) {
+                if (resp.errno == "0") {  // 请求成功的话，更新父窗口中两处昵称显示
+                    $('.user_center_name', parent.document).html(params['nick_name']);
+                    $('#nick_name', parent.document).html(params['nick_name']);
+                    $('.input_sub').blur()
+                }else {
+                    alert(resp.errmsg)
+                }
+            }
+        })
     })
-})
+});
+
